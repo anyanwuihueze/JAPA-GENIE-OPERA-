e0667f24ae64fb8ea433e8d48aaf93120bab703a
DIBIA AI <dibia.aiagency@gmail.com>
2025-08-01 13:17:56 +0000

    firebaseConfig = {   apiKey: "AIzaSyDoH0-oVYmHOlRcfgzLwUBPuXe99eKURww",

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,10 +1,10 @@
 // src/lib/firebase/config.ts
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDoH0-oVYmHOlRcfgzLwUBPuXe99eKURww",
+  authDomain: "visaflow-s1a93.firebaseapp.com",
+  projectId: "visaflow-s1a93",
+  storageBucket: "visaflow-s1a93.appspot.com",
+  messagingSenderId: "794702894126",
+  appId: "1:794702894126:web:7b85520885551f59a4bbbf",
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
